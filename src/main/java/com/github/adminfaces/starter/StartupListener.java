64c2fcccd9d0517461431354b6b0dd8e8577c036
rmpestano <rmpestano@gmail.com>
2019-06-02 10:53:27 -0300

    update to AdminFaces 1.0.2

--- src/main/java/com/github/adminfaces/starter/StartupListener.java
+++ src/main/java/com/github/adminfaces/starter/StartupListener.java
@@ -1,12 +1,10 @@
 package com.github.adminfaces.starter;
 
 import com.github.adminfaces.template.session.AdminFilter;
-import com.github.adminfaces.template.session.AdminServletContextListener;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebListener;
 
 @WebListener
@@ -19,15 +17,10 @@ public class StartupListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        sce.getServletContext().setInitParameter("javax.faces.PROJECT_STAGE", projectStage);
+        sce.getServletContext().setInitParameter("javax.faces.PROJECT_STAGE", "Production");
         sce.getServletContext().setInitParameter("primefaces.THEME", "admin");
         sce.getServletContext().setInitParameter("primefaces.FONT_AWESOME", "true");
         sce.getServletContext().setInitParameter("javax.faces.FACELETS_RESOURCE_RESOLVER", "com.github.adminfaces.starter.CustomResourceResolver");
-        try {
-            sce.getServletContext().createListener(AdminServletContextListener.class);
-        } catch (ServletException e) {
-            e.printStackTrace();
-        }
         sce.getServletContext().addFilter("AdminFilter", AdminFilter.class);
     }
 
